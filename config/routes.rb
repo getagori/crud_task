b3f30279d70d4f3cf9a8f67d222a0ce388d2621c
koudai <57044519+getagori@users.noreply.github.com>
2020-02-19 09:17:13 +0900

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
-  get '/users', to: 'users#index'
-  get '/users/new', to: 'users#new'
-  get '/users/:id/edit', to: 'users#edit'
-  post '/users', to: 'users#create'
-  delete '/users/:id', to: 'users#destroy'
-  patch '/users/:id', to: 'users#update'
+  resources :users
 end
